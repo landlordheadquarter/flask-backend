267bda7ea9804f59659d9be20ba58ca5e85b9211
marlonpd <send8heremd@gmail.com>
2025-05-09 10:34:48 +0800

    Add e bill model

---
+++ landlordhq/electricity_bill/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+"""The electricity module."""
+from . import controller  # noqa
---
+++ landlordhq/electricity_bill/model.py
@@ -0,0 +1,21 @@
+from landlordhq.extensions import db
+from landlordhq.constants import UTILITY_TYPES
+
+class ElectricityBill(db.Model):
+    __tablename__ = 'electricity_bills'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    tenant_id = db.Column(db.Integer, db.ForeignKey('tenants.id'), nullable=False)
+    previous_reading_id = db.Column(db.Integer, db.ForeignKey('meter_readings.id'), nullable=False)
+    current_reading_id = db.Column(db.Integer, db.ForeignKey('meter_readings.id'), nullable=False)
+    reading = db.Column(db.Float, nullable=False)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+    
+    # Relationships
+    user = db.relationship('User', back_populates='electricity_bills')
+    tenant = db.relationship('Tenant', back_populates='electricity_bills')
+    previous_reading = db.relationship('MeterReading', foreign_keys=[previous_reading_id])
+    current_reading = db.relationship('MeterReading', foreign_keys=[current_reading_id])
+    
